6d3da1dadf63666b2141ffd3b5a1e1196957f2b0
Francesco Conoscenti <francesco.conoscenti@studio.unibo.it>
2025-03-28 12:02:26 +0100

    u

--- 1D_LSTM_TP.py
+++ 1D_LSTM_TP.py
@@ -502,6 +502,7 @@ if __name__ == "__main__":
     print(f"num_processes = {num_processes}")
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
     # Create data loaders
     train_loader, test_loader, X_train, X_test, y_train, y_test = create_data_loaders(
     detection_array_ordered, observable_flips, batch_size, test_size)
