e5273f6ef430beb250490c5784b65223d132adc5
Francesco Conoscenti <francesco.conoscenti@studio.unibo.it>
2025-03-28 12:26:25 +0100

    u

--- 1D_LSTM_TP.py
+++ 1D_LSTM_TP.py
@@ -492,7 +492,7 @@ if __name__ == "__main__":
     num_epochs = 1
     fc_layers_intra = [0] #now is not taken into account, there is not hidden layers.
     fc_layers_out = [int(hidden_size/8)]
-    num_processes = 4
+    num_processes = 8
 
     # Print configuration
     print(f"1D LSTM TP")
