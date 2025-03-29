a968fd0b959ad1a4d6037fa97507327249989065
Francesco Conoscenti <francesco.conoscenti@studio.unibo.it>
2025-03-29 13:55:59 +0100

    u

--- try_multiple_gpus.py
+++ try_multiple_gpus.py
@@ -39,7 +39,7 @@ class Trainer:
     def _run_batch(self, source, targets):
         self.optimizer.zero_grad()
         output = self.model(source)
-        loss = F.cross_entropy(output, targets)
+        loss = output.sum() # F.cross_entropy(output, targets)
         loss.backward()
         self.optimizer.step()
 
@@ -66,7 +66,7 @@ class Trainer:
 
 
 def load_train_objs():
-    num_samples = 256
+    num_samples = 128
     input_size = 20
     output_size = 1
 
