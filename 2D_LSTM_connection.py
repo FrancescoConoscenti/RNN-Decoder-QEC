7b5c995a5237486fb6a0bf6914179510af9907ff
Francesco Conoscenti <francesco.conoscenti@studio.unibo.it>
2025-05-21 12:57:24 +0200

    u

--- 2D_LSTM_connection.py
+++ 2D_LSTM_connection.py
@@ -676,12 +676,12 @@ if __name__ == "__main__":
     output_size = 1
     grid_height = 4
     grid_width = 2
-    batch_size = 64
+    batch_size = 128
     test_size = 0.2
     learning_rate = 0.0001
     learning_rate_fine = 0.0001
     dropout_prob = 0.2
-    num_epochs = 1
+    num_epochs = 10
     num_epochs_fine = 5
     fc_layers_intra = [0]
     fc_layers_out = [hidden_size]
