ccb56f22ac0f8c50c5fb3fbd1250db5fdf3f81fa
Francesco Conoscenti <francesco.conoscenti@studio.unibo.it>
2025-05-22 11:47:58 +0200

    u

--- 2D_LSTM.py
+++ 2D_LSTM.py
@@ -479,13 +479,13 @@ observable_flips = observable_flips.astype(int).flatten().tolist()
 
 # Model hyperparameters
 input_size = 1
-hidden_size = 128
+hidden_size = 64
 output_size = 1
 grid_height = 4
 grid_width = 2
-batch_size = 128
+batch_size = 256
 test_size = 0.2
-learning_rate = 0.0005
+learning_rate = 0.0001
 num_epochs = 10
 fc_layers_intra = [0] #not used
 fc_layers_out = [hidden_size]
