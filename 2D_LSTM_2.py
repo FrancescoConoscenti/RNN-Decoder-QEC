265f48a8988193532f8ad8dcb9a14e1853617a07
Francesco Conoscenti <francesco.conoscenti@studio.unibo.it>
2025-05-28 14:17:01 +0200

    u

--- 2D_LSTM_2.py
+++ 2D_LSTM_2.py
@@ -460,7 +460,7 @@ def main():
 
     distance = 3
     rounds = 11
-    num_shots = 1000000
+    num_shots = 2000000
 
     # Configuration
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -481,7 +481,7 @@ def main():
     batch_size = 256  # Reduced batch size
     test_size = 0.2
     learning_rate = 0.0005  # Increased learning rate
-    patience = 5  # Early stopping patience
+    patience = 10  # Early stopping patience
     num_epochs = 50
     fc_layers_out = [hidden_size//2]  # Smaller output layers
     dropout_rate = 0.2
